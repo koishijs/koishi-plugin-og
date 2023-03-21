e002ec88fe628780305b208b22d15cc6a0c0e1d9
Shigma <shigma10826@gmail.com>
2023-03-22 02:41:16 +0800

    feat: add implementation

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { Computed, Context, Dict, h, Schema } from 'koishi'
+import { load } from 'cheerio'
+
+export interface Config {
+  strict?: Computed<boolean>
+}
+
+export const Config: Schema<Config> = Schema.object({
+  strict: Schema.computed(Schema.boolean()).default(false).description('仅匹配只含链接的消息。'),
+})
+
+export const name = 'OpenGraph'
+
+export function apply(ctx: Context, config: Config) {
+  ctx.on('message', async (session) => {
+    const regex = session.resolve(config.strict)
+      ? /^https?:\/\/\S+$/g
+      : /https?:\/\/\S+/g  
+    const match = session.content.trim().match(regex)
+    if (!match) return
+    match.forEach(async (url) => {
+      try {
+        const { data, headers } = await ctx.http.axios(url)
+        if (!headers['content-type']?.startsWith('text/html')) return
+        const $ = load(data)
+        const og = $('meta[property^="og:"]').toArray().reduce((prev, meta) => {
+          const key = meta.attribs.property.slice(3)
+          const value = meta.attribs.content
+          if (value) prev[key] = value
+          return prev
+        }, {} as Dict<string>)
+        if (og.image) {
+          await session.send(h('image', { url: og.image }))
+        }
+      } catch {}
+    })
+  })
+}
