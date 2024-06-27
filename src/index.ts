c0d72d82dc492d7e801f349b08cdee15a5a576bc
jexjws <hello_cmd@outlook.com>
2024-06-28 00:24:05 +0800

    feat: multiple links (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Computed, Context, Dict, h, Schema } from 'koishi'
+import { Computed, Context, Dict, h, Logger, Schema } from 'koishi'
 import { load } from 'cheerio'
 
 export interface Config {
@@ -12,10 +12,12 @@ export const Config: Schema<Config> = Schema.object({
   strict: Schema.computed(Schema.boolean()).default(false).description('仅匹配只含链接的消息。'),
   ignored: Schema.array(Schema.string()).description('忽略特定域名的链接。'),
   sendTitle: Schema.boolean().default(false).description('同时发送标题。'),
-  sendSiteName: Schema.boolean().default(false).description('标题不包含站点名的话，就发送站点名。'),
+  sendSiteName: Schema.boolean().default(false).description('标题和原链接域名均不包含站点名的话，就发送站点名。'),
 })
 
 export const name = 'OpenGraph'
+
+const logger = new Logger('og')
 
 export function apply(ctx: Context, config: Config) {
   ctx.on('message', async (session) => {
@@ -25,11 +27,16 @@ export function apply(ctx: Context, config: Config) {
     const match = session.content.trim().match(regex)
     if (!match) return
 
-    match.forEach(async (url) => {
+    logger.debug('提取出的链接：', match)
+    let message = ''
+    match.forEach(async (url: string) => {
       if (config.ignored?.some((prefix) => url.startsWith(prefix))) return
       try {
         const { data, headers } = await ctx.http(url, { responseType: 'text' })
-        if (!headers.get('content-type')?.startsWith('text/html')) return
+        if (!headers.get('content-type')?.startsWith('text/html')) {
+          logger.debug(`从 ${url} 上抓取下来的数据的 content-type 不是 text/html。终止对该链接的处理流程。`)
+          return
+        }
 
         const $ = load(data)
         const og = $('meta[property^="og:"]').toArray().reduce((prev, meta) => {
@@ -38,16 +45,18 @@ export function apply(ctx: Context, config: Config) {
           if (value) prev[key] = value
           return prev
         }, {} as Dict<string>)
-        let message = ''
-        if (og.site_name && config.sendSiteName && !og.title.toLowerCase().includes(og.site_name.toLowerCase()))
-          message += `${og.site_name}: `
+        let ogblock = ''
+        if (og.site_name && config.sendSiteName && !og.title.toLowerCase().includes(og.site_name.toLowerCase()) && !(new URL(url)).host.toLowerCase().includes(og.site_name.toLowerCase()))
+          ogblock += `${og.site_name} - `
         if (og.title && config.sendTitle)
-          message += `${og.title}`
+          ogblock += `${og.title}`
         if (og.image)
-          message += h('img', { src: new URL(og.image, url).href })
-        if (message !== '')
-          await session.send(message)
+          ogblock += h('img', { src: new URL(og.image, url).href })
+        logger.debug(`从 ${url} 生成了预览：${ogblock}`)
+        if (ogblock !== '')
+          message += `${ogblock}\n`
       } catch {}
     })
+    await session.send(message)
   })
 }
